f183fcf9f536e10333a6ff18b56eb58303da6ead
Leandro Cruz <leandro.programmer@hotmail.com>
2021-01-30 23:01:27 -0300

    add news div on Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 
-import { Container, Header, BackIcon, ProfileInfo } from './styles';
+import { Container, Header, Link, ProfileInfo, Content, Left, Right } from './styles';
 
 const Main: React.FC = () => {
   return (
     <Container>
       <Header>
-        <button>
-          <BackIcon />
-        </button>
-
-        <ProfileInfo>
-          <strong>Leandro Cruz</strong>
-          <span>200 QI</span>
-        </ProfileInfo>
-
-        {/* <BottomMenu>
-          <HomeIcon />
-          <SearchIcon />
-          <BellIcon />
-          <EmailIcon />
-        </BottomMenu> */}
+        <Link>
+          <strong>About Me</strong>
+          <strong>Projects</strong>
+          <strong>Contact</strong>
+        </Link>
       </Header>
+      <Content>
+        <Left>
+          <ProfileInfo>
+            <strong>Leandro</strong>
+            <strong>Cruz</strong>
+            <span>Computer Enginner</span>
+          </ProfileInfo>
+        </Left>
+        <Right>
+          <h1>Picture</h1>
+        </Right>
+      </Content>
     </Container>
   );
 };
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -4,11 +4,69 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  width: min(601px, 100%);
+  width: min(1280px, 100%);
 `;
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  position: sticky;
+  top: 0;
+  background: var(--primary);
 
-export const BackIcon = styled.div``;
+  display: flex;
+  align-items: center;
 
-export const ProfileInfo = styled.div``;
+  text-align: left;
+
+  padding: 10px 13px 9px 13px;
+  border-bottom: 1px solid var(--outline);
+`;
+
+export const Link = styled.div`
+  margin-left: 100px;
+  margin-right: 100px;
+
+  display: flex;
+  flex: 1;
+  justify-content: space-between;
+
+  > strong {
+    font-size: 22px;
+
+    cursor: pointer;
+    transition: 0.13s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Left = styled.div`
+  display: flex;
+  width: 30%;
+  height: 100vh;
+`;
+
+export const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center; */
+  /* align-items: center; */
+  > strong {
+    font-size: 70px;
+  }
+
+  > span {
+    font-size: 22px;
+  }
+`;
+
+export const Right = styled.div`
+  display: flex;
+  width: 70%;
+`;
